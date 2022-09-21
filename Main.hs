29286c4cbc31d4550e8b3614a5fda20b4133d49d
Cristiano-Rocha <sdxadev@gmail.com>
2022-09-20 23:40:50 -0300

    base code

---
+++ Main.hs
@@ -0,0 +1,17 @@
+{-
+
+This is a naive algorithm to check if a given pattern exists in the given text
+
+-}
+
+
+import Data.Text as T
+
+
+search :: Text -> Text -> Text
+search d p =
+
+main :: IO()
+main =
+    do
+        search data pattern
